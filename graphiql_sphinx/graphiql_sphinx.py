2d461c69abeca8226f325e5b7437106bac7d9ef9
Tirth Shah <tirths@york.ie>
2025-01-27 14:28:32 +0530

    fix: custom auth

--- graphiql_sphinx/graphiql_sphinx.py
+++ graphiql_sphinx/graphiql_sphinx.py
@@ -9,7 +9,7 @@ class SphinxGraphiQL(Directive):
     required_arguments: int = 0
     optional_arguments: int = 0
     final_argument_whitespace: bool = True
-    option_spec = {"query": str, "response": str, "endpoint": str, "view_only": str, "headers": str}
+    option_spec = {"query": str, "response": str, "endpoint": str, "view_only": str}
 
     GRAPHIQL_TEMPLATE: str = '''
 .. raw:: html
@@ -18,37 +18,49 @@ class SphinxGraphiQL(Directive):
     <script src="https://cdnjs.cloudflare.com/ajax/libs/react/18.0.0/umd/react.production.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.0.0/umd/react-dom.production.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/graphiql/3.8.0/graphiql.min.js"></script>
-    <script
-      src="https://unpkg.com/@graphiql/plugin-explorer/dist/index.umd.js"
-      crossorigin
-    ></script>
-    <link
-      rel="stylesheet"
-      href="https://unpkg.com/@graphiql/plugin-explorer/dist/style.css"
-    />
+    <script src="https://unpkg.com/@graphiql/plugin-explorer/dist/index.umd.js" crossorigin></script>
+    <link rel="stylesheet" href="https://unpkg.com/@graphiql/plugin-explorer/dist/style.css" />
 
     <div id="graphiql" style="height: 80vh; width:120vh; padding-left: 1vh"></div>
 
     <script>
-        const graphQLFetcher = (graphQLParams) => {
-            console.log("Endpoint: {{ endpoint }}");
+        const graphQLFetcher = async (graphQLParams, options = {}) => {
+            let headers = {};
+            
+            if (typeof options.headers === 'string') {
+                try {
+                    headers = JSON.parse(options.headers);
+                } catch (e) {
+                    console.log('Error parsing headers:', e);
+                }
+            } else if (options.headers) {
+                headers = options.headers;
+            }
+            
+            console.log("Headers being sent:", headers);
+            
             return fetch('{{ endpoint }}', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
-                    ...JSON.parse('{{ headers }}')
+                    ...headers
                 },
                 body: JSON.stringify(graphQLParams),
             }).then(response => response.json());
         };
+
         const explorerPlugin = GraphiQLPluginExplorer.explorerPlugin();
+        
         ReactDOM.render(
-        React.createElement(GraphiQL, { 
-            fetcher: graphQLFetcher,
-            defaultEditorToolsVisibility: true,
-            plugins: [explorerPlugin],
-        }),
-        document.getElementById('graphiql')
+            React.createElement(GraphiQL, {
+                fetcher: graphQLFetcher,
+                defaultEditorToolsVisibility: true,
+                plugins: [explorerPlugin],
+                defaultHeaders: JSON.stringify({
+                    'Authorization': 'Bearer your-token-here'
+                }, null, 2)
+            }),
+            document.getElementById('graphiql')
         );
     </script>
     '''
